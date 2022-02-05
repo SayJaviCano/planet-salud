c6e5983361090e9166d4445d400af0e33fc6f490
davidcanonieto <david.cano.nieto@gmail.com>
2022-02-05 19:29:04 +0000

    Tests para pagina de registro

---
+++ cypress/constants/texts.js
@@ -0,0 +1,16 @@
+export const textosRegistro = {
+  tituloFormulario: 'Nuevo cliente',
+  camposFormulario: {
+    nombre: 'Nombre',
+    apellidos: 'Apellidos',
+    nif: 'DNI/NIF:',
+    telefono: 'Teléfono:',
+    direccion: 'Dirección',
+    cp: 'Código postal',
+    poblacion: 'Población',
+    provincia: 'Provincia',
+    email: 'E-Mail:',
+    clave: 'Contraseña:',
+    acepto_politica: 'Acepto Aviso legal y política de privacidad',
+  },
+};
